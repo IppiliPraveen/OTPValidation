8b34d2ebaf723ab6f162e99946bb9bbc94f06e80
Praveen Ippili <Praveen199936@gmail.com>
2024-11-01 20:18:09 +0530

    Validation service added

--- OTPValidation/src/main/java/com/praveen/spring/PraveenSpringApplication.java
+++ OTPValidation/src/main/java/com/praveen/spring/PraveenSpringApplication.java
@@ -1,17 +1,13 @@
 package com.praveen.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.praveen.spring.service.EmailSenderService;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.praveen"})
+//@EnableJpaRepositories(basePackages = "com.praveen.spring.repository")
 public class PraveenSpringApplication {
 
-	@Autowired
-	EmailSenderService emailSenderService;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PraveenSpringApplication.class, args);
 	}
--- OTPValidation/src/main/java/com/praveen/spring/repository/EmailOTPValidationRepository.java
+++ OTPValidation/src/main/java/com/praveen/spring/repository/EmailOTPValidationRepository.java
@@ -3,9 +3,11 @@ package com.praveen.spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.praveen.spring.entity.OTP;
 
+@Repository
 public interface EmailOTPValidationRepository extends JpaRepository<OTP, Long>{
 
 	@Query(name = "OTP.findLatestValidOtpByEmail")
