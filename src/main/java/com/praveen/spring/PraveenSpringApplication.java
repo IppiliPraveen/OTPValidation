51bcbba2abd9da8f0dc47cd9cb6ab28c84021655
Praveen Ippili <Praveen199936@gmail.com>
2024-11-03 16:11:57 +0530

    Speeling changes

--- src/main/java/com/praveen/spring/PraveenSpringApplication.java
+++ src/main/java/com/praveen/spring/PraveenSpringApplication.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication(scanBasePackages = {"com.praveen"})
 //@EnableJpaRepositories(basePackages = "com.praveen.spring.repository")
-@OpenAPIDefinition(info = @Info(title = "OTP API", version = "1.0", description = "OTP Vefifivation API"))
+@OpenAPIDefinition(info = @Info(title = "OTP API", version = "1.0", description = "OTP Verification API"))
 public class PraveenSpringApplication {
 
 	public static void main(String[] args) {
